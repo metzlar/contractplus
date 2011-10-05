22544fe003bb6a60263a9597bda7b754774f828b
Berco Beute <berco@bairco.(none)>
2011-10-05 09:42:30 +0200

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+
+from setuptools import setup, find_packages
+
+if 'register' in sys.argv or 'upload' in sys.argv:
+    raise Exception('I don\'t want to be on PyPI!')
+
+setup(
+    name='contract',
+    description='contract forked from https://github.com/barbuza/contract',
+    license='none',
+    version='1.0',
+    author='barbuza',
+    author_email='',
+    packages=find_packages(),
+    include_package_data=True,
+    )
