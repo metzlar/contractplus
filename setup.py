c2cce5eba2046cda7724665d5d1c77faf63b252d
Ivan Metzlar <metzlar@gmail.com>
2011-10-14 14:56:24 +0200

    New version.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name='contractplus',
     description='contract forked from https://github.com/barbuza/contract',
     license='none',
-    version='1.0',
+    version='1.1',
     author='barbuza',
     author_email='',
     py_modules=['contract'],
